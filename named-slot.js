da84b661dfa004505e435ed0ae3fd6746537e6f3
Johann Tutor <johann.tutor@groupbyinc.com>
2019-05-23 13:30:06 -0400

    Add comments to NamedSlot

--- named-slot.js
+++ named-slot.js
@@ -1,7 +1,19 @@
 import { LitElement, html } from 'lit-element';
 
+/* NamedSlot contains three slots: one unnamed and two named.
+ */
 class NamedSlot extends LitElement {
   render() {
+    /* Slots can be named. A node in the light DOM can be marked for
+     * distribution into a slot using the `slot` attribute. Any
+     * non-marked nodes, including text and whitespace nodes, get
+     * distributed into the unnamed slot.
+     *
+     * Multiple nodes can be marked for the same slot, and can be
+     * interspersed with unmarked nodes or nodes marked for different
+     * slots. Each node will be distributed into its respective slot and
+     * appended to the slot in order.
+     */
     return html`
       <div>
         Unnamed: |<slot>Unnamed</slot>|<br />
