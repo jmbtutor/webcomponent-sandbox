14766259982800a2015b23a81b5336fc9d71e924
Johann Tutor <johann.tutor@groupbyinc.com>
2019-05-23 10:54:56 -0400

    Add comments to HardCodedList

--- hard-coded-list.js
+++ hard-coded-list.js
@@ -1,6 +1,17 @@
 import { LitElement, html } from 'lit-element';
 
+/* HardCodedList is a simple LitElement component. It only contains a
+ * `render` function that has a fixed template.
+ */
 class HardCodedList extends LitElement {
+  /* LitElement uses the `html` template tag from lit-html to transform
+   * a string representation into a performant HTML template.
+   * The `html` template tag produces a lit-html TemplateResult, which
+   * contains a reference to the underlying Template object and various
+   * other things.
+   *
+   * The nodes are rendered in a shadow DOM by default.
+   */
   render() {
     return html`
       <ul>
@@ -12,4 +23,7 @@ class HardCodedList extends LitElement {
   }
 }
 
+/* The custom element must be registered with the browser's custom
+ * elements registry.
+ */
 window.customElements.define('hard-coded-list', HardCodedList);
