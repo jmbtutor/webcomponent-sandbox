ba8303e5343e7e4c80645df74496bd132bc4a586
Johann Tutor <johann.tutor@groupbyinc.com>
2019-05-23 10:58:08 -0400

    Add comments to SingleSlotWithDefault

--- single-slot-with-default.js
+++ single-slot-with-default.js
@@ -1,6 +1,15 @@
 import { LitElement, html } from 'lit-element';
 
-class SingleSlot extends LitElement {
+/* SingleSlotWithDefault is like SingleSlot, but the slot contains a
+ * fallback value.
+ */
+class SingleSlotWithDefault extends LitElement {
+  /* A <slot> can have child nodes, and these child nodes serve as the
+   * fallback nodes in case the host element does not have any child
+   * nodes in the light DOM. That is, the children of <slot> here will
+   * display if <single-slot-with-default> does not have any children
+   * when it's used in the light DOM.
+   */
   render() {
     return html`
       <p>Before slot</p>
@@ -10,4 +19,4 @@ class SingleSlot extends LitElement {
   }
 }
 
-window.customElements.define('single-slot-with-default', SingleSlot);
+window.customElements.define('single-slot-with-default', SingleSlotWithDefault);
