bab20daf92676ac907b545c74b90f0d0c7fb658b
Johann Tutor <johann.tutor@groupbyinc.com>
2019-05-23 10:55:10 -0400

    Add comments to SingleSlot

--- single-slot.js
+++ single-slot.js
@@ -1,6 +1,13 @@
 import { LitElement, html } from 'lit-element';
 
+/* SingleSlot is a LitElement component that allows slotted content through an unnamed slot.
+ */
 class SingleSlot extends LitElement {
+  /* When using a shadow DOM, the component can take children and slot
+   * them into a template using the <slot> element. Any child nodes in
+   * the light DOM, including whitespace nodes, will be inserted at the
+   * point where the <slot> is.
+   */
   render() {
     return html`
       <p>Before slot</p>
